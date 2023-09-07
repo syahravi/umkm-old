b3f8698033b642b8954477183b7b9af3a5947705
Saeful mu'minin <saeful2026027@gmail.com>
2023-09-07 18:13:18 +0700

    tambah form create

--- resources/views/admin/umkm/create.blade.php
+++ resources/views/admin/umkm/create.blade.php
@@ -3,15 +3,15 @@
 @section('title', 'Add UMKM')
 
 @section('content')
-    <div class="bg-blue-500 py-4">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="py-4 bg-blue-500">
+        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="flex justify-end space-x-4">
                 <a href="{{ route('admin.umkm.index') }}"
-                    class="inline-block bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-400 text-white font-medium rounded-lg text-base px-8 py-4">
+                    class="inline-block px-8 py-4 text-base font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-400">
                     UMKM List
                 </a>
                 <a href="{{ route('admin.umkm.create') }}"
-                    class="inline-block bg-green-500 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-400 text-white font-medium rounded-lg text-base px-8 py-4">
+                    class="inline-block px-8 py-4 text-base font-medium text-white bg-green-500 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-400">
                     Add UMKM
                 </a>
             </div>
@@ -19,15 +19,15 @@
     </div>
 
     <div class="py-10 bg-gray-100">
-        <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-2xl px-4 mx-auto sm:px-6 lg:px-8">
             <form action="{{ route('admin.umkm.store') }}" method="POST" enctype="multipart/form-data"
-                class="bg-white shadow rounded-lg px-8 py-6 space-y-4">
+                class="px-8 py-6 space-y-4 bg-white rounded-lg shadow">
                 @csrf
                 <div class="flex flex-col">
-                    <h1 class="text-3xl text-center  mb-6">Tambah UMKM</h1>
+                    <h1 class="mb-6 text-3xl text-center">Tambah UMKM</h1>
                     <label for="nama" class="text-sm font-medium text-gray-700">Nama UMKM</label>
                     <input type="text" name="nama_umkm" id="nama"
-                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('nama') border-red-500 @enderror"
+                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('nama') @enderror"
                         value="{{ old('nama') }}" required>
                     @error('nama')
                         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
@@ -36,32 +36,32 @@
                 <div class="flex flex-col">
                     <label for="deskripsi" class="text-sm font-medium text-gray-700">Deskripsi UMKM</label>
                     <textarea name="deskripsi" id="deskripsi"
-                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('deskripsi') border-red-500 @enderror"
+                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('deskripsi') @enderror"
                         rows="5" required>{{ old('deskripsi') }}</textarea>
                     @error('deskripsi')
                         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="flex flex-col">
-                    <label for="gambar_umkm" class="text-sm font-medium text-gray-700">Gambar UMKM</label>
-                    <input type="file" name="gambar_umkm" id="gambar_umkm"
-                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('gambar_umkm') border-red-500 @enderror"
-                        required>
-                    @error('gambar_umkm')
-                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="flex flex-col">
-                    <label for="gambar_produk" class="text-sm font-medium text-gray-700">Gambar Produk</label>
-                    <input type="file" name="gambar_produk" id="gambar_produk"
-                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('gambar_produk') border-red-500 @enderror"
+                    <label for="gambar_produk" class="text-sm font-medium text-gray-700">Link WA</label>
+                    <input type="text" name="gambar_produk" id="gambar_produk"
+                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('gambar_produk') @enderror"
                         required>
                     @error('gambar_produk')
                         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="flex justify-center items-center">
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-400 text-white font-medium rounded-lg text-base px-8 py-4 w-full">
+                <div class="flex flex-col">
+                    <label for="gambar_umkm" class="text-sm font-medium text-gray-700">Gambar UMKM</label>
+                    <input type="file" name="gambar_umkm" id="gambar_umkm"
+                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('gambar_umkm') @enderror"
+                        required>
+                    @error('gambar_umkm')
+                        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex items-center justify-center">
+                    <button type="submit" class="w-full px-8 py-4 text-base font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-400">
                         Simpan
                     </button>
                 </div>
