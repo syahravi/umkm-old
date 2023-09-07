3c18938c7321afaa9c93a38f3e092f993e49f2ce
Saeful mu'minin <saeful2026027@gmail.com>
2023-09-07 18:13:44 +0700

    nambar dan total product

--- resources/views/admin/umkm/index.blade.php
+++ resources/views/admin/umkm/index.blade.php
@@ -1,43 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto py-8">
-        <h1 class="text-3xl md:text-5xl text-center font-bold mb-4">Daftar UMKM
+    <div class="container py-8 mx-auto">
+        <h1 class="mb-4 text-3xl font-bold text-center md:text-5xl">Daftar UMKM
             <a href="{{ route('admin.umkm.create') }}"
-            class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg mb-4">
+            class="inline-block px-4 py-2 mb-4 font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600">
             Tambah UMKM
         </a>
         </h1>
         
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             @foreach ($umkms as $umkm)
-                <div class="border rounded-lg p-4">
-                    <div class="text-center mb-2">
+                <div class="p-4 border rounded-lg shadow-md">
+                    <div class="mb-2 text-center">
                         <img src="{{ asset('/storage/' . $umkm->id . '/' . $umkm->gambar_umkm) }}"
-                            alt="{{ $umkm->nama_umkm }}" class="w-24 h-24 object-cover rounded-lg mx-auto">
+                            alt="{{ $umkm->nama_umkm }}" class="object-cover w-full h-32 mx-auto rounded-lg">
                     </div>
                     <div class="mb-2">
                         <span class="font-semibold">Nama:</span>
                         {{ $umkm->nama_umkm }}
                     </div>
-                    <div class="mb-2">
+                    <div class="mb-2 ">
                         <span class="font-semibold">Deskripsi:</span>
-                        {{ $umkm->deskripsi }}
+                        <p class="w-auto h-16 overflow-y-auto">{{ $umkm->deskripsi }}</p>
+                        
                     </div>
                     <div class="flex justify-center">
                         <a href="{{ route('admin.umkm.show', $umkm) }}"
-                            class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-1 px-2 rounded-lg mx-1">
+                            class="inline-block px-2 py-1 mx-1 font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600">
                             Lihat
                         </a>
                         <a href="{{ route('admin.umkm.edit', $umkm) }}"
-                            class="inline-block bg-green-500 hover:bg-green-600 text-white font-medium py-1 px-2 rounded-lg mx-1">
+                            class="inline-block px-2 py-1 mx-1 font-medium text-white bg-green-500 rounded-lg hover:bg-green-600">
                             Edit
                         </a>
                         <form action="{{ route('admin.umkm.destroy', $umkm) }}" method="POST" class="inline">
                             @csrf
                             @method('DELETE')
                             <button type="submit"
-                                class="inline-block bg-red-500 hover:bg-red-600 text-white font-medium py-1 px-2 rounded-lg mx-1"
+                                class="inline-block px-2 py-1 mx-1 font-medium text-white bg-red-500 rounded-lg hover:bg-red-600"
                                 onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">
                                 Hapus
                             </button>
