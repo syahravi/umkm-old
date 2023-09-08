19261f608d024c27a6d4b5a06b0f538cb1baf634
Syahravi <syahravi.id@gmail.com>
2023-09-08 23:15:45 +0700

    create

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\UMKM;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -18,9 +21,10 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create($umkm_id)
     {
-        return view('admin.umkm.produk.create');
+        $umkm = UMKM::findOrFail($umkm_id);
+        return view('admin.umkm.produk.create', compact('umkm'));
     }
 
     /**
@@ -28,7 +32,36 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock'=> 'required|numeric',
+            'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $product = Product::create([
+            'name' => $request->name,
+            'description'=> $request->description,
+            'price'=> $request->price,
+            'stock'=> $request->stock,
+            'umkm_id' => $request->umkm_id,
+        ]);
+
+        // Thumbnail
+        $thumbnail = $request->file('thumbnail');
+        $thumbnailName = time() . '.' . $thumbnail->extension();
+        $thumbnail->move(public_path('storage/UMKM/' . $request->umkm_id . '/product'), $thumbnailName);
+        $product->thumbnail = '/storage/UMKM/' . $request->umkm_id . '/product/' . $thumbnailName;
+        $product->save();
+
+        return redirect()->back();
     }
 
     /**
