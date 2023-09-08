3587461a47e0c9f3d1479cb94726c6fa68b96b87
Syahravi <syahravi.id@gmail.com>
2023-09-08 23:16:48 +0700

    Fungsi buat

--- resources/views/admin/umkm/produk/create.blade.php
+++ resources/views/admin/umkm/produk/create.blade.php
@@ -3,55 +3,56 @@
 @section('content')
 <div class="py-10 bg-gray-100">
     <div class="max-w-2xl px-4 mx-auto sm:px-6 lg:px-8">
-        <form action="{{ route('admin.umkm.store') }}" method="POST" enctype="multipart/form-data"
+        <form action="{{ route('admin.produk.store', $umkm->id) }}" method="POST" enctype="multipart/form-data"
             class="px-8 py-6 space-y-4 bg-white rounded-lg shadow">
             @csrf
+            <input type="hidden" name="umkm_id" value="{{ $umkm->id }}">
             <h1 class="mb-6 text-3xl text-center">Tambah Produk</h1>
             
             <div class="flex flex-col">
-                <label for="nama" class="text-sm font-medium text-gray-700">Nama produk</label>
-                <input type="text" name="nama_umkm" id="nama"
-                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('nama') @enderror"
-                    value="{{ old('nama') }}" required>
-                @error('nama')
+                <label for="name" class="text-sm font-medium text-gray-700">Nama Produk</label>
+                <input type="text" name="name" id="name"
+                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('name') @enderror"
+                    value="{{ old('name') }}" required>
+                @error('name')
                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                 @enderror
             </div>
             
             <div class="flex flex-col">
-                <label for="deskripsi" class="text-sm font-medium text-gray-700">Deskripsi produk</label>
-                <textarea name="deskripsi" id="deskripsi"
-                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('deskripsi') @enderror"
-                    rows="5" required>{{ old('deskripsi') }}</textarea>
-                @error('deskripsi')
+                <label for="description" class="text-sm font-medium text-gray-700">Deskripsi Produk</label>
+                <textarea name="description" id="description"
+                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('description') @enderror"
+                    rows="5" required>{{ old('description') }}</textarea>
+                @error('description')
                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                 @enderror
             </div>
             
             <div class="flex flex-col">
-                <label for="harga_produk" class="text-sm font-medium text-gray-700">Harga produk</label>
-                <input type="number" name="harga_produk" id="harga_produk"
-                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('harga_produk') @enderror"
+                <label for="price" class="text-sm font-medium text-gray-700">Harga Produk</label>
+                <input type="number" name="price" id="price"
+                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('price') @enderror"
                     required>
-                @error('harga_produk')
+                @error('price')
                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                 @enderror
             </div>
             <div class="flex flex-col">
-                <label for="sisa_produk" class="text-sm font-medium text-gray-700">Sisa Produk</label>
-                <input type="number" name="sisa_produk" id="sisa_produk"
-                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('sisa_produk') @enderror"
+                <label for="stock" class="text-sm font-medium text-gray-700">Sisa Produk</label>
+                <input type="number" name="stock" id="stock"
+                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('stock') @enderror"
                     required>
-                @error('sisa_produk')
+                @error('stock')
                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                 @enderror
             </div>
             <div class="flex flex-col">
-                <label for="gambar_produk" class="text-sm font-medium text-gray-700">Gambar produk</label>
-                <input type="file" name="gambar_produk" id="gambar_produk"
-                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('gambar_produk') @enderror"
+                <label for="thumbnail" class="text-sm font-medium text-gray-700">Gambar produk</label>
+                <input type="file" name="thumbnail" id="thumbnail"
+                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 @error('thumbnail') @enderror"
                     required>
-                @error('gambar_produk')
+                @error('thumbnail')
                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                 @enderror
             </div>
