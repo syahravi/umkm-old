2dbbbf9282ed3a555d4b1b41c246fef329fae306
Saeful mu'minin <saeful2026027@gmail.com>
2023-09-09 09:00:50 +0700

    dasboard

--- resources/views/umkm/show.blade.php
+++ resources/views/umkm/show.blade.php
@@ -68,7 +68,7 @@
         </div>
     </section>
     {{-- footer --}}
-    <section class="py-12 bg-gradient-to-r from-fuchsia-600 to-blue-600">
+    {{-- <section class="py-12 bg-gradient-to-r from-fuchsia-600 to-blue-600">
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
             <div class="text-center xl:flex xl:items-center xl:justify-between xl:text-left">
                 <div class="xl:flex xl:items-center xl:justify-start">
@@ -113,5 +113,5 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 </x-guest-layout>
