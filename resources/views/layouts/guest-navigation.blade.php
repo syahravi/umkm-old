b7ae81edc2fb18c65c891ffb57902fb1cc2fe1ae
Saeful mu'minin <saeful2026027@gmail.com>
2023-09-09 11:06:56 +0700

    comit nafbar

--- resources/views/layouts/guest-navigation.blade.php
+++ resources/views/layouts/guest-navigation.blade.php
@@ -1,53 +1,38 @@
-<header class="fixed z-50 w-full bg-white shadow-md">
-    <div class="px-4 mx-auto sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16 lg:h-20">
-            <div class="flex-shrink-0">
-                <a href="/" title="" class="flex">
-                    <img class="w-auto h-14" src="{{ asset('images/umkmm.png') }}" alt="cimulang" />
-                </a>
-            </div>
+<div x-data="{ open: false }">
+    <!-- Navbar -->
+    <nav class="bg-white shadow-md p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <a href="#" class="text-white text-2xl font-semibold">
+                <img src="{{asset('images/umkmm.png')}}" alt="umkm cimulang" class="w-auto h-10">
+            </a>
 
-            <button type="button"
-                class="inline-flex p-1 text-black transition-all duration-200 border border-black lg:hidden focus:bg-gray-100 hover:bg-gray-100">
-                <!-- Menu open: "hidden", Menu closed: "block" -->
-                <svg class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            <!-- Tombol Menu Mobile -->
+            <button @click="open = !open" class="lg:hidden text-white focus:outline-none">
+                <svg x-show="!open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
-
-                <!-- Menu open: "block", Menu closed: "hidden" -->
-                <svg class="hidden w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
-                    </path>
+                <svg x-show="open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
             </button>
 
-            <div class="hidden ml-auto lg:flex lg:items-center lg:justify-center lg:space-x-10">
-                <a href="/" title=""
-                    class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"> Beranda
-                </a>
-
-                <a href="/#tentangkami" title=""
-                    class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80">
-                    Tentang </a>
-
-                <a href="/#produk" title=""
-                    class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"> Produk
-                    UMKM</a>
-
-                <a href="#hubungikami" title=""
-                    class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80">
-                    Hubungi Kami </a>
-{{-- 
-                <div class="w-px h-5 bg-black/20"></div>
-
-                <a href="{{ route('login') }}" title=""
-                    class="text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80"> Masuk
-                </a> --}}
-            </div>
+            <!-- Menu Utama -->
+            <ul class="hidden lg:flex space-x-4">
+                <li><a href="#" class="text-dark hover:text-blue-600 transition-colors duration-300 font-sans font-bold">Home</a></li>
+                <li><a href="#tentangkami" class="text-dark hover:text-blue-600 transition-colors duration-300 font-sans font-bold">Tentang Kami</a></li>
+                <li><a href="#produk" class="text-dark hover:text-blue-600 transition-colors duration-300 font-sans font-bold">Produk Umkm</a></li>
+                <li><a href="#hubungikami" class="text-dark hover:text-blue-600 transition-colors duration-300 font-sans font-bold">Hubungi Kami</a></li>
+            </ul>
         </div>
+    </nav>
+
+    <!-- Menu Mobile -->
+    <div x-show="open" class="lg:hidden bg-gradient-to-r from-blue-500 to-teal-400 p-4">
+        <ul class="space-y-4">
+            <li><a href="#"  class="text-white hover:text-teal-400 transition-colors duration-300 font-sans font-bold">Home</a></li>
+            <li><a href="#tentangkami"  class="text-white hover:text-teal-400 transition-colors duration-300 font-sans font-bold">Tentang Kami</a></li>
+            <li><a href="#produk"  class="text-white hover:text-teal-400 transition-colors duration-300 font-sans font-bold">Produk Umkm</a></li>
+            <li><a href="#hubungikami"  class="text-white hover:text-teal-400 transition-colors duration-300 font-sans font-bold">Hubungi Kami</a></li>
+        </ul>
     </div>
-</header>
-<div class="bg-gradient-to-b from-green-50 to-green-100">
 </div>
